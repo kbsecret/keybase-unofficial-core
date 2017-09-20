97e5393f17ed778daf7e93203c76269fe1cd93d4
William Woodruff <william@tuffbizz.com>
2017-09-19 22:45:53 -0400

    core: Update documentation

--- lib/keybase/core.rb
+++ lib/keybase/core.rb
@@ -3,8 +3,11 @@
 require_relative "core/exceptions"
 require_relative "core/u"
 
+# The primary namespace.
 module Keybase
+  # The namespace for `keybase-unofficial-core`.
   module Core
+    # The current version of `keybase-unofficial-core`.
     VERSION = "1.0.0"
   end
 end
--- lib/keybase/core/exceptions.rb
+++ lib/keybase/core/exceptions.rb
@@ -2,7 +2,9 @@
 
 module Keybase
   module Core
+    # A namespace for all exceptions defined by {Keybase::Core}.
     module Exceptions
+      # The top level exception class.
       class KeybaseError < RuntimeError
       end
     end
